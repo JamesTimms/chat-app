b0fca613caeab5e4fff8e5896d6f21329c86ee83
James Timms <james.timms@wayhome.co.uk>
2023-04-28 14:18:40 +0100

    Update demo to use a Connection Manager
    Add a Connection Manager that enables multiple clients to connection. Broadcast all messages to all clients.

--- server/src/server.py
+++ server/src/server.py
@@ -1,16 +1,31 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-
+from src.connection_manager import ConnectionManager
+import traceback
+import json
 
 app = FastAPI()
 
+connection_manager = ConnectionManager()
 
 @app.websocket("/messaging")
 async def websocket_endpoint(websocket: WebSocket):
-  await websocket.accept()
-  try:
-    while True:
-        data = await websocket.receive_text()
-        print(f"Received data {data}")
-        await websocket.send_text(data)
-  except WebSocketDisconnect:
-      print(f"client disconnected")
+    # Accept the connection from the client
+    await connection_manager.connect(websocket)
+    try:
+        while True:
+            # Receive the message from the client
+            data = await websocket.receive_text()
+            print("Received: ", data)
+            # Send the message to all the clients
+            await connection_manager.broadcast(data)
+
+    except WebSocketDisconnect:
+        print('WebSocketDisconnect Exception...')
+        traceback.print_exc()
+
+    finally:
+        # Remove the connection from the list of active connections
+        id = connection_manager.disconnect(websocket)
+        # Broadcast the disconnection of client with id to all the clients
+        print(f'Client disconnected {id}')
+        await connection_manager.broadcast(json.dumps({"type": "disconnected", "id": id}))
